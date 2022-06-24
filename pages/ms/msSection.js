20afe1528bc48c387b221646a43cf36dd38d3ba1
Jason Tamez <jasontamez@users.noreply.github.com>
2022-06-24 00:56:18 -0500

    MS checkboxes

--- pages/ms/msSection.js
+++ pages/ms/msSection.js
@@ -1,20 +1,7 @@
-import React from 'react';
-import {
-	IonPage,
-	IonContent,
-	IonList,
-	useIonViewDidEnter
-} from '@ionic/react';
-import {
-	SyntaxHeader
-} from './MorphoSyntaxElements';
-import { changeView } from '../../components/ReduxDucksFuncs';
 import { useDispatch } from "react-redux";
-
 import ms from './ms.json';
-//import doParse from 'html-react-parser';
 import { useParams } from 'react-router-dom';
-import { Modal, Slider, Text, TextArea, VStack } from 'native-base';
+import { Checkbox, HStack, Modal, ScrollView, Slider, Text, TextArea, VStack } from 'native-base';
 import {
 	setSyntaxBool,
 	setSyntaxNum,
@@ -29,6 +16,82 @@ const parseMSJSON = (page) => {
 		1: "1.4rem",
 		2: "1.25rem",
 		3: "1.1rem"
+	};
+	// Text formatting
+	const formatText = (bit, makeBold=false) => {
+		let temp = [bit];
+		let temp2 = [];
+		// Check for BOLD text
+		temp.forEach((t) => {
+			if(typeof t === "string") {
+				let toggle = false;
+				t.split("**").forEach((x) => {
+					if(x === "") {
+						// Just skip.
+						return;
+					} else if (toggle) {
+						temp2.push(<Text bold>{x}</Text>);
+						toggle = false;
+					} else {
+						temp2.push(x);
+						toggle = true;
+					}
+				});
+			}
+		});
+		temp = [...temp2];
+		temp2 = [];
+		// Check for ITALIC text
+		temp.forEach((t) => {
+			if(typeof t === "string") {
+				let toggle = false;
+				t.split("//").forEach((x) => {
+					if(x === "") {
+						// Just skip.
+						return;
+					} else if (toggle) {
+						temp2.push(<Text italic>{x}</Text>);
+						toggle = false;
+					} else {
+						temp2.push(x);
+						toggle = true;
+					}
+				});
+			}
+		});
+		temp = [...temp2];
+		temp2 = [];
+		// Check for STRIKE-THROUGH text
+		temp.forEach((t) => {
+			if(typeof t === "string") {
+				let toggle = false;
+				t.split("--").forEach((x) => {
+					if(x === "") {
+						// Just skip.
+						return;
+					} else if (toggle) {
+						temp2.push(<Text strikeThrough>{x}</Text>);
+						toggle = false;
+					} else {
+						temp2.push(x);
+						toggle = true;
+					}
+				});
+			}
+		});
+		// If only one line of text, present it simply
+		if(temp2.length === 1) {
+			const oneLine = temp2[0];
+			let final;
+			if(typeof oneLine === "string") {
+				final = <Text bold={makeBold}>{oneLine}</Text>;
+			} else {
+				final = oneLine;
+			}
+			return final;
+		}
+		// Multi-lined texts
+		return <Text bold={makeBold}>{...temp2}</Text>;
 	};
 	return doc.map((bit) => {
 		const tag = (bit.tag || "");
@@ -70,6 +133,9 @@ const parseMSJSON = (page) => {
 				);
 			case "Modal":
 				const synState = useSelector((state) => state.morphoSyntaxModalState);
+				//
+				// BEGIN MODAL CONTENT DECLARATION
+				//
 				const ModalContent = (content) => {
 					const regularMargin = 2;
 					const noMargin = 0;
@@ -80,6 +146,9 @@ const parseMSJSON = (page) => {
 					let indent = 0;
 					let unindent = 0;
 					let unindentStorage = [];
+					//
+					// BEGIN WHILE LOOP
+					//
 					while(input.length > 0) {
 						let bit = input.shift();
 						// Begin tree
@@ -108,90 +177,17 @@ const parseMSJSON = (page) => {
 							input.unshift(...newContent);
 						} else if (typeof bit === "string") {
 							// Make a simple text
-							let temp = [bit];
-							let temp2 = [];
-							temp.forEach((t) => {
-								if(typeof t === "string") {
-									let toggle = false;
-									t.split("**").forEach((x) => {
-										if(x === "") {
-											// Just skip.
-											return;
-										} else if (toggle) {
-											temp2.push(<Text bold>{x}</Text>);
-											toggle = false;
-										} else {
-											temp2.push(x);
-											toggle = true;
-										}
-									});
-								}
-							});
-							temp = [...temp2];
-							temp2 = [];
-							temp.forEach((t) => {
-								if(typeof t === "string") {
-									let toggle = false;
-									t.split("//").forEach((x) => {
-										if(x === "") {
-											// Just skip.
-											return;
-										} else if (toggle) {
-											temp2.push(<Text italic>{x}</Text>);
-											toggle = false;
-										} else {
-											temp2.push(x);
-											toggle = true;
-										}
-									});
-								}
-							});
-							temp = [...temp2];
-							temp2 = [];
-							temp.forEach((t) => {
-								if(typeof t === "string") {
-									let toggle = false;
-									t.split("--").forEach((x) => {
-										if(x === "") {
-											// Just skip.
-											return;
-										} else if (toggle) {
-											temp2.push(<Text strikeThrough>{x}</Text>);
-											toggle = false;
-										} else {
-											temp2.push(x);
-											toggle = true;
-										}
-									});
-								}
-							});
-							if(temp2.length === 1) {
-								const oneLine = temp2[0];
-								let final;
-								if(typeof oneLine === "string") {
-									final = <Text>{oneLine}</Text>;
-								} else {
-									final = oneLine;
-								}
-								output.push(
-									<Box mt={margin} pl={String(indent * 2)+"rem"}>
-										{final}
-									</Box>
-								);
-							} else {
-								output.push(
-									<Box mt={margin} pl={String(indent * 2)+"rem"}>
-										<Text>{...temp2}</Text>
-									</Box>
-								);
-							};
+							output.push(
+								<Box mt={margin} pl={String(indent * 2)+"rem"}>
+									{formatText(bit)}
+								</Box>
+							);
 							// Reset margin, if needed
 							margin = regularMargin;
 						} else {
 							// some type of object - currently only have the tabular object so I won't bother checking
-							const makeLine = (inputRows) => {
-								let rows = [];
-								inputRows.forEach(ir => rows.push([...ir]));
+							// Make a vertical stack of all row elements
+							const makeLine = (rows) => {
 								return (
 									<VStack m={2} alignItems="center" justifyContent="space-between">
 										{rows.map((r) => {
@@ -204,7 +200,7 @@ const parseMSJSON = (page) => {
 								<ScrollView horizontal mt={margin} pl={String(indent * 2)+"rem"}>
 									<VStack>
 										<HStack>
-											{data.map(item => makeLine(bit.rows))}
+											{data.map(() => makeLine([...bit.rows]))}
 										</HStack>
 										{bit.final ? <Text>{bit.final}</Text> : ""}
 									</VStack>
@@ -241,17 +237,19 @@ const parseMSJSON = (page) => {
 							}
 						}
 					}
-					return (
-						<>
-							{...output}
-						</>
-					);
+					//
+					// END WHILE LOOP
+					//
+					return <>{...output}</>;
 				};
+				//
+				// END MODAL CONTENT DECLARATION
+				//
 				return (
 					<>
 						<Modal size="5/6" m="auto" isOpen={synState[id] !== undefined} onClose={() => dispatch(setSyntaxState(id, false))}>
 							<Modal.CloseButton />
-							<Modal.Header>{bit.title}</Modal.Header>
+							<Modal.Header><Text>{bit.title}</Text></Modal.Header>
 							<Modal.Body>
 								<VStack>
 									<ModalContent content={bit.content} />
@@ -265,100 +263,65 @@ const parseMSJSON = (page) => {
 					</>
 				);
 			case "Checkboxes":
-				const disp = bit.display;
+				const synBool = useSelector((state) => state.morphoSyntaxInfo.bool);
+				const setBool = (what, value) => {
+					dispatch(setSyntaxBool(what, value));
+				};				const disp = bit.display;
 				if(!disp) {
-					return "<div>CHECKBOX DISPLAY ERROR</div>";
+					return <Box><Text bold>CHECKBOX DISPLAY ERROR</Text></Box>;
 				}
-				const boxes = (bit.boxes || []);
+				const boxes = (bit.boxes || []).slice();
 				const rowLabels = (disp.rowLabels || []).slice();
 				const perRow = disp.boxesPerRow;
 				const labels = (disp.labels || []).slice();
 				const header = disp.header;
 				const inlineHeaders = disp.inlineHeaders;
+				// Assemble tabular section
+				//
+				// Start with the checkboxes
+				let cols = [];
 				let count = 0;
-				let rows = [];
-				let temp = [];
-				boxes.forEach((box) => {
+				// Make X arrays of checkboxes, where X is boxesPerRow
+				while(count < perRow) {
+					cols.push([]);
 					count++;
-					temp.push(box || "Error");
-					if(count >= perRow) {
-						temp.push(rowLabels.shift() || "Error");
-						count = 0;
-						rows.push(temp);
-						temp = [];
-					}
-				});
-				const printRow = (row, cn = "") => {
-					const label = row.pop() || "";
-					return (
-						<IonRow className={cn || undefined}>
-							{row.map((c) => <IonCol className="cbox"><RadioBox prop={String(c)} /></IonCol>)}
-							<IonCol>{doParse(label)}</IonCol>
-						</IonRow>
-					);
-				};
-				const printRowWithLabel = (row, cn = "") => {
-					const final = row.pop() || "";
-					const label = labels.shift() || "";
-					const labelClass = disp.labelClass || "label"
-					return (
-						<IonRow className={cn || undefined}>
-							{row.map((c) => <IonCol className="cbox"><RadioBox prop={String(c)} /></IonCol>)}
-							<IonCol className={labelClass}>{doParse(label)}</IonCol>
-							<IonCol>{doParse(final)}</IonCol>
-						</IonRow>
-					);
-				};
-				const printHeaderRow = (row, hasLabel = false) => {
-					const final = row.pop() || "";
-					let label = "";
-					if(hasLabel) {
-						label = row.pop();
-					}
-					return (
-						<IonRow className="header">
-							{row.map((c) => <IonCol className="cbox">{c}</IonCol>)}
-							{label ? <IonCol className={disp.labelClass || "label"}>{doParse(label)}</IonCol> : label}
-							<IonCol>{doParse(final)}</IonCol>
-						</IonRow>
-					);
-				};
+				}
+				// Iterate over the columns until no boxes remain
+				while(boxes.length > 0) {
+					cols.forEach((col) => {
+						const id = boxes.shift();
+						col.push(id ? <Checkbox value={id} onChange={(val) => setBool(what, val)} defaultIsChecked={synBool[what] || false} /> : <Text>ERROR</Text>);
+					});
+				}
+				// If labels exist, add them as a new column
+				if(labels) {
+					cols.push(labels.map((l) => formatText(l)));
+				}
+				// Add rowlabels
+				cols.push(rowLabels.map(rl => formatText(rl)));
+				// Add inlineheaders, if any
+				if(inlineHeaders) {
+					cols.forEach(col => {
+						const header = inlineHeaders.shift() || "ERROR";
+						// Force headers to be bold
+						col.unshift(formatText(header, true));
+					});
+				}
+				// Put it all together
 				return (
-					<IonItem className="content">
-						<IonGrid className={disp.class || undefined}>
-							{header ? <IonRow  className="header"><IonCol>{doParse(header)}</IonCol></IonRow> : ""}
-							{inlineHeaders ? printHeaderRow(inlineHeaders.slice(), !!disp.labels) : ""}
-							{rows.map((r) => disp.labels ? printRowWithLabel(r.slice()) : printRow(r.slice()))}
-						</IonGrid>
-					</IonItem>
+					<ScrollView horizontal>
+						<VStack>
+							{header ? <Box>{formatText(header, true)}</Box> : ""}
+							<HStack>
+								{cols.map(col => <VStack>{col.map(c => <Box>{c}</Box>)}</VStack>)}
+							</HStack>
+						</VStack>
+					</ScrollView>
 				);
 		}
+		// No switch matched?
 		return "";
 	});
-};
-
-const OldSection = () => {
-	const dispatch = useDispatch();
-	const viewInfo = ['ms', 'ms01'];
-	useIonViewDidEnter(() => {
-		dispatch(changeView(viewInfo));
-	});
-
-	const { page } = useParams();
-	const n = page.slice(-2);
-	const pageNum = Number(page.slice(-2));
-	const pageName = "s" + n;
-
-	return (
-		<IonPage>
-			<SyntaxHeader title={ms.titles[pageNum]} />
-			<IonContent fullscreen className="evenBackground disappearingHeaderKludgeFix" id="morphoSyntaxPage">
-				<IonList lines="none">
-					{parseMSJSON(pageName)}
-				</IonList>
-			</IonContent>
-		</IonPage>
-	);
 };
 
 const Section = () => {
