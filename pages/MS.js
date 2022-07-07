ec877dc71520898c6b92cacda5836188ed266cac
Jason Tamez <jasontamez@users.noreply.github.com>
2022-07-07 00:26:55 -0500

    *MS navbar changes colors based on page

--- pages/MS.js
+++ pages/MS.js
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 //import { shallowEqual, useSelector } from "react-redux";
 import { Ionicons } from '@expo/vector-icons';
 
@@ -11,24 +11,26 @@ const MS = () => {
 	//const msPage = useSelector((state) => state.viewState.ms, shallowEqual) || "msSettings";
 	const navigate = useNavigate();
 	const NavTab = (props) => {
-		// TO-DO:
-		// if page == props.link, render as outline instead of ghost
-		const variant = "ghost";
-		if(props.icon) {
+		const { isCurrent, icon, link, label } = props;
+		const bg = isCurrent ? "lighter" : "transparent";
+		const colorString = isCurrent ? "primary.500" : "text.50";
+		const textOptions = isCurrent ? {bold: true, color: colorString} : {color: colorString}
+		if(icon) {
 			return (
-				<IconButton minWidth={6} variant={variant} color="text.50" onPress={() => navigate(props.link)} icon={props.icon} _icon={{size: "md", color: "text.50"}} />
+				<IconButton minWidth={6} variant="ghost" bg={bg} color={colorString} onPress={() => navigate(link)} icon={icon} _icon={{size: "md", color: colorString}} />
 			);
 		}
 		return (
-			<Button minWidth={8} variant={variant} color="text.50" _text={{color: "text.50"}} onPress={() => navigate(props.link)}>{props.label}</Button>
+			<Button minWidth={8} variant="ghost" bg={bg} color={colorString} _text={textOptions} onPress={() => navigate(link)}>{label}</Button>
 		);
 	};
 	const NavTabs = () => {
+		const location = useLocation();
 		let range = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
 		return (
 			<NavBar boxProps={{flexGrow: 0, borderColor: "main.700", borderTopWidth: 1}}>
-				<NavTab icon={<Icon as={Ionicons} name="settings-sharp" size="2xs" />} link="/ms" key="NavTabSettings" />
-				{range.map((n, i) => <NavTab label={String(i+1)} link={"ms" + n} key={"Tab"+n} />)}
+				<NavTab isCurrent={location.pathname === "/ms"} icon={<Icon as={Ionicons} name="settings-sharp" size="2xs" />} link="/ms" key="NavTabSettings" />
+				{range.map((n, i) => <NavTab isCurrent={location.pathname === "/ms/ms" + n} label={String(i+1)} link={"ms" + n} key={"Tab"+n} />)}
 			</NavBar>
 		);
 	};
